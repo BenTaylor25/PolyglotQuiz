8d962eb498812c0161107ed94cd11410f8080b54
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-23 21:47:27 +0000

    added Python questions

--- Python/quiz.py
+++ Python/quiz.py
@@ -1,12 +1,29 @@
 
-questions = [
-    { 
-        'q': "In what year was Python first released?", 
-        'a': "1991"
-    },
+questions = [ 
     {
+        'q': "In what year was Python 1.0 released?", 
+        'a': "1991"
+    }, {
+        'q': "In what year was Python 2.0 released?",
+        'a': "2000"
+    }, {
+        'q': "In what year was Python 3.0 released?",
+        'a': "2008"
+    }, {
         'q': "Who is the creator of Python?",
         'a': "Guido van Rossum"
+    }, {
+        'q': "What language was Python designed as a successor to?",
+        'a': "ABC"
+    }, {
+        'q': "How many reserved keywords does Python have?",
+        'a': "33"
+    }, {
+        'q': "How do you denote a single-line comment in Python?",
+        'a': "#"
+    }, {
+        'q': "What is the standard typing style for variables in Python?",
+        'a': "snake_case"
     }
 ]
 
