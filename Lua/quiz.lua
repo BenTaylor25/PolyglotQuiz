8cbea6cee64e8f1c36cc89fde431b708e295b9b6
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-24 16:52:22 +0000

    Lua solution

--- Lua/quiz.lua
+++ Lua/quiz.lua
@@ -7,5 +7,22 @@ function main()
 
     local incorrect_guesses = 0
 
+    for _, question in pairs(questions) do
+        print(question['q'])
+
+        local guess = ""
+        while guess ~= question['a'] and guess ~= "skip" do
+            io.write("> ")
+            guess = io.read("*l")
+
+            if guess == "skip" then
+                incorrect_guesses = incorrect_guesses + 10
+            elseif guess ~= question['a'] then
+                incorrect_guesses = incorrect_guesses + 1
+            end
+        end
+    end
+
+    print("Incorrect Guesses: " .. tostring(incorrect_guesses))
 end
 main()
