9aca92171e8ff90e308e9dfdc88c12277a64b5ad
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-28 11:00:54 +0000

    Go solution

--- Go/quiz.go
+++ Go/quiz.go
@@ -1,14 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
 
 func main() {
 	var questions []map[string]string
 
-	questions = append(questions, make(map[string]string))
-	questions[0]["q"] = "q1"
-	questions[0]["a"] = "a1"
+	questions = append(questions, map[string]string { "q": "q1", "a": "a1" })
+	questions = append(questions, map[string]string { "q": "q2", "a": "a2" })
 
-	fmt.Println(questions[0]["q"])
+	incorrectGuesses := 0
+
+	for _, question := range questions {
+		fmt.Println(question["q"])
+
+		guess := ""
+		reader := bufio.NewReader(os.Stdin)
+		for guess != question["a"] && guess != "skip" {
+			fmt.Print("> ")
+			guess, _ = reader.ReadString('\n')
+			guess = strings.Replace(guess, "\n", "", -1)
+
+			if guess == "skip" {
+				incorrectGuesses += 10
+				fmt.Println(question["a"])
+				fmt.Println()
+			} else if guess != question["a"] {
+				incorrectGuesses++
+			} else {
+				fmt.Println("Correct")
+				fmt.Println()
+			}
+		}
+
+	}
+
+	fmt.Println(incorrectGuesses)
 
 }
