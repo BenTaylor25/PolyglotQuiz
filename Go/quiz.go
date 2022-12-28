f111984dc17ee389765669ab86f65f73c4bc892a
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-28 11:10:34 +0000

    added Go questions

--- Go/quiz.go
+++ Go/quiz.go
@@ -10,8 +10,11 @@ import (
 func main() {
 	var questions []map[string]string
 
-	questions = append(questions, map[string]string { "q": "q1", "a": "a1" })
-	questions = append(questions, map[string]string { "q": "q2", "a": "a2" })
+	questions = append(questions, map[string]string { "q": "In what year was the Go project first started?", "a": "2007" })
+	questions = append(questions, map[string]string { "q": "In what year was Go 1.0 released?", "a": "2009" })
+	questions = append(questions, map[string]string { "q": "What organisation created Go?", "a": "Google" })
+	questions = append(questions, map[string]string { "q": "Go was primarily created because people didn't like which language?", "a": "C++" })
+	questions = append(questions, map[string]string { "q": "What famous development container program was written in Go?", "a": "Docker" })
 
 	incorrectGuesses := 0
 
