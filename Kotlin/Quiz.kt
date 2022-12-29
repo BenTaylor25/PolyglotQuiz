df116054f0eefdd2a143c9bf007aee341a9a149d
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-29 14:09:27 +0000

    Kotlin solution

--- Kotlin/Quiz.kt
+++ Kotlin/Quiz.kt
@@ -1,3 +1,4 @@
+import java.util.*
 
 data class QuestionAnswer (
     val q: String,
@@ -6,11 +7,33 @@ data class QuestionAnswer (
 
 fun main() {
 
-    var question = arrayOf(
+    var questions = arrayOf(
         QuestionAnswer("q1", "a1"),
         QuestionAnswer("q2", "a2")
     );
 
+    var incorrectGuesses = 0
 
-    println(question[0].q)
+    for (question in questions) {
+        println(question.q)
+
+        var guess = ""
+        while (guess != question.a && guess != "skip") {
+            print("> ")
+            guess = readLine()!!
+
+            if (guess == "skip") {
+                incorrectGuesses += 10
+                println(question.a)
+                println()
+            } else if (guess != question.a) {
+                incorrectGuesses++
+            } else {
+                println("Correct")
+                println()
+            }
+        }
+    }
+
+    println("Incorrect Guesses: " + incorrectGuesses.toString())
 }
