71fc2ff35187d78e7ca340c56b73c2abc497dcc4
benlaptop25 <ben.taylor2573@gmail.com>
2023-09-10 14:21:46 +0100

    wip Zig

--- Zig/quiz.zig
+++ Zig/quiz.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
 const QuestionAnswer = struct {
     q: []const u8,
@@ -19,8 +20,30 @@ pub fn main() !void {
         }
     };
 
+    var incorrect_guesses = 0;
+
     for (questions) |question| {
         try stdout.print("{s}\n", .{question.q});
-        try stdout.print("{s}\n\n", .{question.a});
+
+        var guess = "";
+
+        while (guess != "skip" and guess != question.a) {
+            try stdout.print("> ");
+
+            var buf: [100]u8 = undefined;
+            guess = try stdin.readUntilDelimiterOrEof(buf[0..], '\n') orelse "";
+
+            // fix string comparison - std.mem.eql()?
+            if (guess == "skip") {
+                incorrect_guesses += 10;
+                stdout.print("{s}\n", .{question.a});
+            } else if (guess != question.a) {
+                incorrect_guesses += 1;
+            } else {
+                stdout.print("Correct\n", .{});
+            }
+        }
     }
+
+    stdout.print("Incorrect Guesses: {i}", incorrect_guesses);
 }
