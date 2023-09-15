fd0504c26973df51a3d356a25708f7dd9d36da43
benlaptop25 <ben.taylor2573@gmail.com>
2023-09-15 07:24:23 +0100

    try again later

--- Zig/quiz.zig
+++ Zig/quiz.zig
@@ -25,28 +25,58 @@ pub fn main() !void {
     for (questions) |question| {
         try stdout.print("{s}\n", .{question.q});
 
-        var guess = "";   // doesn't like this combined with line 35...
+        //var guess = "";   // doesn't like this combined with line 35...
 
-        // while (guess != "skip" and guess != question.a) {
-        while (!std.mem.eql(u8, guess, "skip") and !std.mem.eql(u8, guess, question.a)) {
+        // while (!std.mem.eql(u8, guess, "skip") and !std.mem.eql(u8, guess, question.a)) {
+        while (true) {
             try stdout.print("> ", .{});
 
-            var buf: [100]u8 = undefined;
-            guess = try stdin.readUntilDelimiterOrEof(buf[0..], '\n') orelse "";
+            const guess = try readLine(stdin);
 
             // fix string comparison - std.mem.eql()?
             // if (guess == "skip") {
             if (std.mem.eql(u8, guess, "skip")) {
                 incorrect_guesses += 10;
                 stdout.print("{s}\n", .{question.a});
+                break;
             // } else if (guess != question.a) {
             } else if (!std.mem.eql(u8, guess, question.a)) {
                 incorrect_guesses += 1;
             } else {
                 stdout.print("Correct\n", .{});
+                break;
             }
         }
     }
 
     try stdout.print("Incorrect Guesses: {s}\n", .{incorrect_guesses});
+}
+
+// A combination of minimal online resources and
+// minimal time is making Zig a lot more difficult
+// than the other languages in this repo.
+// Zig is a very interesting language, and I would
+// like to revisit this challenge some time in the
+// near future.
+
+fn readLine(reader: *std.io.File.Reader) ![]const u8 {
+    var buffer: [256]u8 = undefined;
+    var line: []const u8 = undefined;
+
+    while (true) {
+        const bytesRead = try reader.read(buffer[0..]);
+
+        if (bytesRead == 0) {
+            break;
+        }
+
+        for (buffer[0..bytesRead]) |byte| {
+            if (byte == '\n' || byte == '\r') {
+                line = buffer[0..];
+                return line;
+            }
+        });
+    }
+
+    return line;
 }
